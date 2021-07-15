ee34177e5dd86643dfc236c43b5cd61831df4588
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-07-14 23:39:23 -0700

    add hamburger button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import './style.css';
+import './hamburger.css'
+
 const addButton = document.querySelector('#addButton');
 const addTaskButton = document.querySelector('.createTaskButtonContainer');
 const addProjectButton = document.querySelector('.createProjectButtonContainer');
+const hamburgerButton = document.querySelector('.hamburger');
+
 
 addButton.addEventListener('click', () => {
     changeStatusOfButtons();
@@ -11,3 +16,9 @@ function changeStatusOfButtons() {
     addTaskButton.classList.toggle('active');
     addProjectButton.classList.toggle('active');
 }
+
+hamburgerButton.addEventListener('click', () => {
+    hamburgerButton.classList.toggle('is-active');
+    console.log('asd')
+})
+
