577cd87b746c4699e004870b2fb8b4883993d34f
toan95dn <51420692+toan95dn@users.noreply.github.com>
2021-07-25 22:41:14 -0700

    add event listeners for sign up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import './hamburger.css';
 import './login.css';
 import 'normalize.css'
+import './login';
 import { TaskModel, TaskView, TaskController } from './Task';
 import { TasksManagerModel } from './TasksManager';
 import { pubsub } from './pubsub';
--- src/login.js
+++ src/login.js
@@ -1,9 +1,71 @@
-: root {
-    --main - bg - color:#e76f51;
-    --task - bg - color: #f4a261;
-    --header - bg - color:#264653;
-    --menu - bg - color: #2a9d8f;
-    --taskdone - bg - color: lightslategrey;
+const createLogin = (() => {
+    // const loginScreen = document.createElement('div');
+    // loginScreen.classList.add('loginContainer');
 
-}
+    // loginScreen.innerHTML = `
+    //     <div class='imageContainer'>
+    //         <img src="/src/todoImage.svg" alt="">
+    //     </div>
 
+
+    //     <form class='signInForm' action="">
+    //         <div class="brandName"><span>T</span>
+    //             <span class="material-icons">
+    //                 task_alt
+    //             </span>
+    //             <span>DO</span>
+    //             <span class="material-icons">
+    //                 sentiment_very_satisfied
+    //             </span>
+    //         </div>
+
+    //         <h2>Organize your life!</h2>
+
+    //         <div class='emailpassInput'>
+    //             <label for="email">Email</label>
+    //             <input type="email" name="email" id="email" required>
+    //         </div>
+
+    //         <div class='emailpassInput'>
+    //             <label for="password">Password</label>
+    //             <input type="password" name="password" id="password" required>
+    //         </div>
+
+    //         <div class='signInUpButtons'><button id='signInButton'><span>Sign In</span></button>
+    //             <button id='signUpButton'><span></span>Sign Up</button>
+    //         </div>
+
+    //         <div class='or'>-OR-</div>
+
+    //         <div class='logInWithGGDemoContainer'>
+    //             <button id='loginGoogleButton'><span>Sign In With Google</span></button>
+    //             <button id='loginDemoButton'><span>Sign In With Demo</span></button>
+    //         </div>
+    //     </form>
+    //     `
+    // document.querySelector('body').append(loginScreen);
+})()
+
+
+const addSignUpEvent = (() => {
+    const signUpButton = document.querySelector('#signUpButton');
+    const signUpContainer = document.querySelector('.signUpContainer');
+    const logInContainer = document.querySelector('.logInInputContainer');
+    const cancelSignUpButton = document.querySelector('#cancelSignUpButton');
+
+    function switchSignUpAndLogIn() {
+        signUpContainer.classList.toggle('active');
+        logInContainer.classList.toggle('active');
+    }
+
+    signUpButton.addEventListener('click', () => {
+        switchSignUpAndLogIn();
+    })
+
+    cancelSignUpButton.addEventListener('click', () => {
+        switchSignUpAndLogIn();
+    })
+
+})()
+
+export { createLogin }
